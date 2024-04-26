d7d0338bd8335c67ddec8405364dd65a5696eca7
Rasel Kibria <mdraselkibria11@gmail.com>
2024-04-26 07:39:34 +0600

    book,chapter route added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = require("./src/route/api");
-const connectDB = require("./src/database/DBConnect")
+const connectDB = require("./src/database/DBConnect");
 const app = new express();
 const bodyParser = require("body-parser");
 
@@ -11,10 +11,6 @@ const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 const cors = require("cors");
 
-//Database
-const mongoose = require("mongoose");
-const path = require("path");
-
 //Security Middleware Implement
 app.use(cors());
 app.use(helmet());
@@ -24,18 +20,12 @@ app.use(hpp());
 //Body perser
 app.use(bodyParser.json());
 //mongodb Connection
-connectDB()
+connectDB();
 
 //Rate Limiter
 const limiter = rateLimit({ windowMs: 15 * 60 * 100, max: 3000 });
-app.use(limiter)
-//Database
+app.use(limiter);
 // Managing BackEnd API Routing
-app.use("/api/v1", router);
-// Managing Front End Routing
-app.use(express.static("client/dist"));
-app.get("*", function (res, res) {
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-});
+app.use(router);
 
 module.exports = app;
--- src/route/api.js
+++ src/route/api.js
@@ -1,11 +1,23 @@
-const {
-  CreateProduct,
-  ReadProduct,
-  UpdateProduct,
-  DeleteProduct,
-  GetProductById,
-} = require("../controller/ProductController");
-// const upload = require("../helper/multerConfig")
 const router = require("express").Router();
+const Book = require("../models/bookModel");
+const Chapter = require("../models/chapterModel");
+
+router.get("/books", async (req, res) => {
+  try {
+    const data = await Book.find();
+    res.status(200).json({ status: true, data: data });
+  } catch (error) {
+    res.status(200).json({ status: false, error: error });
+  }
+});
+
+router.get("/chapter", async (req, res) => {
+  try {
+    const data = await Chapter.find();
+    res.status(200).json({ status: true, data: data });
+  } catch (error) {
+    res.status(200).json({ status: false, error: error });
+  }
+});
 
 module.exports = router;
